3434e8a689a88bc1205c84c255845e85310f9474
Unknown <jantono9916@gmail.com>
2018-06-10 11:14:24 -0600

    commit5
    Implementing Match class..

--- Match.py
+++ Match.py
@@ -1,13 +1,13 @@
 class Match:
-    def __init__(self, matchNumber, gamersName, totalPlays, reason):
+    def __init__(self, matchNumber, gamersName, totalPlays):
         self.__matchNumber = matchNumber
         self.__gamersName = gamersName
         self.__totalPlays = totalPlays
-        self.__reason = reason
 
-    def setReason(self, reason):
-        self.__reason = reason
+    def setMatchNumber(self, matchNumber):
+        self.__matchNumber = matchNumber
 
-    def getReason(self):
-        return self.__reason
+    def getMatchNumber(self):
+        return self.__matchNumber
+
 
